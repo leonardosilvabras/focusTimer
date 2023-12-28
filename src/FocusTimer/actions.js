2bc3c93f63656d7a7f48f5d359064a3b10ee8e3f
Leonardo Silva <103190146+leosilvafx@users.noreply.github.com>
2023-12-27 22:56:25 -0300

    funcoes js em modulos

---
+++ src/FocusTimer/actions.js
@@ -0,0 +1,19 @@
+import state from './state.js'
+import * as timer from './timer.js'
+
+export function toggleRunning () {
+  state.isRunning = document.documentElement.classList.toggle('running')
+
+  timer.countdown()
+}
+
+export function reset () {
+  state.isRunning = false;
+  document.documentElement.classList.remove('running')
+}
+
+export function set () {}
+
+export function toggleMusic () {
+  state.isMute = document.documentElement.classList.toggle('music-on')
+}
---
+++ src/FocusTimer/timer.js
@@ -0,0 +1,17 @@
+import state from "./state.js"
+import * as el from './elements.js'
+
+export function countdown () {
+  if (!state.isRunning) { return }
+  else {
+    setTimeout(() => countdown(), 1000)
+  }
+}
+
+export function updateDisplay (minutes, seconds) {
+  minutes = minutes ?? state.minutes
+  seconds = seconds ?? state.seconds
+
+  el.minutes.textContent = String(minutes).padStart(2, '0')
+  el.seconds.textContent = String(seconds).padStart(2, '0')
+}
